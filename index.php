e2e20e85199a37e4cd02a863a76412233b4c5d58
Peter Brinck <peter@ac10.dk>
2015-03-26 20:45:03 +0100

    Droppet idé med billede
    Tilføjet Twitter feed som eksempel
    Bedre responsive

--- index.php
+++ index.php
@@ -43,19 +43,29 @@ require_once("php/global_variables.php");
 
 
 		<?php require_once("php/elements/navigation.php"); ?>
-		<div id="particles-canvas" class="particles">
+		<div id="particles-canvas" class="particles hide-on-mobile">
 
 
 		</div>
 
-		<h1>h1</h1>
-		<h2>h2</h2>
-		<h3>h3</h3>
-		<h4>h4</h4>
-		<h5>h5</h5>
-		<h6>h6</h6>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem impedit hic delectus qui, architecto aperiam repellendus sed sint! Voluptatum, cupiditate enim sequi nesciunt consectetur cum fuga, illum sint doloribus repellat quae! Porro laborum quisquam, nostrum, architecto assumenda quis non. Molestias dignissimos, perferendis tempora odio, hic labore alias fuga quidem nesciunt illum fugiat veritatis delectus ea quas, natus illo, dolorum ipsum soluta architecto dolore necessitatibus. Voluptas quos provident velit similique pariatur dolores repellat dicta consectetur, mollitia eius, autem quo doloremque non, culpa, debitis cumque error deserunt labore ab! Ratione reiciendis ab et impedit quibusdam ipsum debitis, quae laborum aut accusamus possimus rerum perferendis temporibus labore doloribus tenetur, voluptate sit dolorum voluptatem libero soluta. Omnis sunt praesentium, aut accusamus dolor assumenda officiis.</p>
+			<div class="grid">
+				<div class="col-8-12">
+					<div class="content">
+						<h1>Velkommen</h1>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima odit ex, atque deleniti blanditiis, iste, possimus, ullam optio in tempore dolorum aliquid dignissimos ad voluptates quos reiciendis! Culpa quibusdam ex ut velit itaque quis molestiae minus vero, natus perferendis laudantium amet reiciendis ullam, harum officiis! Rerum in omnis, nostrum soluta repellat pariatur, possimus ut delectus. Explicabo culpa, ratione, voluptatum placeat sed, perferendis veniam totam quam, odit fugiat voluptate! Molestias odit fugit mollitia, cum assumenda asperiores necessitatibus adipisci quia vero corporis dignissimos molestiae expedita numquam maiores voluptatibus est earum ipsum saepe ut! Voluptatum adipisci quis possimus, minus eaque quod, neque nostrum culpa explicabo quo nulla. Facilis, dolore. Voluptatum tempore commodi at impedit ut repellendus tempora distinctio, excepturi suscipit, officiis fugit, hic neque nisi. Rerum dicta vitae distinctio officia possimus hic quasi, eius similique minus, laboriosam numquam ex nisi ipsam ducimus error nesciunt repudiandae reiciendis facilis. Dolor error ex deleniti natus nulla, tenetur harum officia eius! Ad similique, dignissimos quibusdam placeat aut!</p>
 
+					</div>
+				</div>
+				<div class="col-4-12 hide-on-mobile">
+					<div class="content">
+						<h1>Socialt</h1>
+						<a class="twitter-timeline"  href="https://twitter.com/RolfBjerre" data-widget-id="581151553777696768">Tweets by @RolfBjerre</a>
+						<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+
+					</div>
+				</div>
+			</div>
 
 
 
