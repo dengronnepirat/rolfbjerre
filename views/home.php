a49f4b4b4061cf73954125c23025148c7bd15b45
Peter Brinck <peter@peterbrinck.dk>
2015-06-04 08:01:10 +0200

    Scrollspy repester nu

--- views/home.php
+++ views/home.php
@@ -13,7 +13,7 @@
 				<div class="uk-width-large-1-3 uk-margin-large-bottom uk-flex rb-circle-wrapper">
 					<div class="uk-panel uk-panel-box uk-text-center rb-circle-panel">
 						<div class="uk-flex uk-flex-center uk-margin">
-							<div class="rb-quick-circle uk-flex uk-flex-center uk-flex-middle" data-uk-scrollspy="{cls:'uk-animation-scale-up'}">
+							<div class="rb-quick-circle uk-flex uk-flex-center uk-flex-middle" data-uk-scrollspy="{cls:'uk-animation-scale-up' , repeat: true}">
 								<i class="uk-icon-<?php echo $circle['icon_slug']; ?> uk-icon-large uk-text-contrast rb-circle-icon"></i>
 							</div>
 						</div>
